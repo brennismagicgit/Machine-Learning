af7644fefe4962f7e1d63a488a542c44605c437c
brennismagicgit <birkner.bernhard@hotmail.com>
2016-06-01 15:48:34 +0200

    Ex 3 finished
    .

--- Assignment1/Exercise3/kmeans.m
+++ Assignment1/Exercise3/kmeans.m
@@ -6,8 +6,7 @@ data = reshape(gesture_,600,3);
 init_cluster = init_cluster_;
 
 n = length(init_cluster);
-N = length(data);
-epochs = 0;
+
 distances = cell(1,n);  % dists of data points to each cluster center
 euklSum = cell(1,n);
 clusters = cell(1,n);
@@ -16,34 +15,26 @@ y = init_cluster;       % 7x3 matrix
 newTotalDistortion = 100;
 decrement = 1;
 
-while(decrement > 10e-6)
+threshold = 10e-6;
+epochs = 0;
+
+while(decrement > threshold)
     epochs = epochs+1;
     for k=1:n
-        dists = zeros(1,N);
-        for j=1:N
-            dists(j) = norm(data(j,:)-y(k,:));
-        end
-        distances{k} = dists';
-    end
-    
-    [v,cluster] = min(cell2mat(distances)');    % find cluster with min dist
-    
+        distances{k} = pdist2(data,y(k,:));
+    end   
+    [v,cluster] = min(cell2mat(distances)');    % find cluster with min dist 
     for k=1:n                                   % add to corresponding cluster
         I = find(cluster ==k);
         clusters{k} = data(I,:);
         y(k,:) = mean(clusters{k});
     end
     for k=1:n                                  % get Total eukl Sum for each cluster
-       eukl=zeros(1,length(clusters{k}));
-       for j=1:length(clusters{k})
-           eukl(j) = norm(clusters{k}(j,:)-y(k,:));
-       end
-       euklSum{k} = sum(eukl); 
+       euklSum{k} = sum(pdist2(clusters{k},y(k,:)));  
     end
     oldTotalDistortion = newTotalDistortion;
     newTotalDistortion = sum(cell2mat(euklSum));
     decrement = abs(oldTotalDistortion-newTotalDistortion);
-    disp(epochs);
 end
 
 end
